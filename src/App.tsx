d33681ee677ff95bc92c03bd9cb1b21ecf433c23
Joao Vitor cardoso <sr.melovitor@gmail.com>
2023-10-01 22:26:53 -0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-
+import { IColaborador } from './compartilhado/interfaces/IColaborador';
+import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
-import Banner from './componentes/Banner';
 
 function App() {
 
@@ -44,16 +44,15 @@ function App() {
     }
   ]
 
-  const [colaboradores, setColaboradores] = useState([])
+  const [colaboradores, setColaboradores] = useState<IColaborador[]>([])
 
-  const aoNovoColaboradorAdicionado = (colaborador) => {
-    debugger
+  const aoNovoColaboradorAdicionado = (colaborador: IColaborador) => {
     setColaboradores([...colaboradores, colaborador])
   }
 
   return (
     <div className="App">
-      <Banner enderecoImagem='/imagens/banner.png' />
+      <Banner enderecoImagem='/imagens/banner.png'/>
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
 
       {times.map(time => <Time 
